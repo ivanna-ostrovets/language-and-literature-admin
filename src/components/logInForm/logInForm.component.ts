3e8bcc6137544fd64a5c6af8d0a4a0c95fe8a4da
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-03-21 19:16:05 +0200

    Add basic log in functionality

--- src/components/logInForm/logInForm.component.ts
+++ src/components/logInForm/logInForm.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'llta-log-in-form',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./logInForm.component.scss']
 })
 export class logInFormComponent {
+  userLogin : string = "admin";
+  password : string = "password";
+
+  inputLogin : string;
+  inputPassword : string;
+
+  showError : boolean = false;
+
+  constructor(
+    private location: Location
+  ) {}
+
+  onSubmit() {
+    if (this.inputLogin == this.userLogin && this.inputPassword == this.password) {
+      this.location.go('/create-test');
+    } else {
+      this.showError = true;
+    }
+  }
 }
