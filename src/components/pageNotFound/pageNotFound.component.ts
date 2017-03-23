2ad904c6af43fb65495b5134020144385a02e1f3
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-03-23 12:52:23 +0200

    Add footer component

--- src/components/pageNotFound/pageNotFound.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './pageNotFound.component.html',
-  // styleUrls: ['./pageNotFound.component.scss']
-})
-export class PageNotFoundComponent {
-
-}
