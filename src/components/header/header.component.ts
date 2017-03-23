b472c22616d7080aedbce948c9fad29a47823de2
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-03-23 12:52:47 +0200

    Add header component

---
+++ src/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'llta-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+}
