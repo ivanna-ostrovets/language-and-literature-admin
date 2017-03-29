6f0bfef76a7ce86b526a5dd2a93590e971d7edaf
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-03-29 22:51:20 +0300

    Add basic functionality to choosing questions number in test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ngOnInit } from '@angular/core';
 
+declare const $: any;
 const range = require('lodash.range');
 
 @Component({
@@ -7,18 +8,54 @@ const range = require('lodash.range');
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  question: string;
-  addImage: boolean;
-  matching: boolean;
-  answersQuantity: number;
+export class AppComponent implements ngOnInit {
+  tasksQuantity: number;
+  currentTab: number = 0;
+  question: string[] = [];
+  addImage: boolean[] = [];
+  matching: boolean[] = [];
+  answersQuantity: number[] = [];
   answers: string[] = [];
   answersTableTitles: string[] = [];
   answersLetters: string[] = [];
   answersNumbers: string[] = [];
 
-  getNumbersRange(): number[] {
-    return range(0, this.answersQuantity);
+  initTabs() {
+    $('ul.tabs').tabs();
+  }
+
+  before(tab: number): void {
+    this.currentTab--;
+    $('ul.tabs').tabs('select_tab', 'tab${currentTab}');
+  };
+
+  next(tab: number): void {
+    this.currentTab++;
+    $('ul.tabs').tabs('select_tab', 'tab${currentTab}');
+  };
+  
+  getNumbersRange(number: number): number[] {
+    return range(0, number);
+  }
+
+  ngOnInit() {
+    $(document).on("keydown", function(event) {
+      if (tasksQuantity > 1) {
+        if ((event.keyCode || event.which) == 37) {
+          if (currentTab > 0) {
+            before(currentTab - 1);
+            return false;
+          }
+        }
+
+        if ((event.keyCode || event.which) == 39) {
+          if (currentTab < 11) {
+            next(currentTab + 1);
+            return false;
+          }
+        }
+      }
+    });
   }
 
   onSubmit() {
