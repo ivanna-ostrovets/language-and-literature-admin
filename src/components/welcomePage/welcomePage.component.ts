7516511cda3d434f6441e5ba8dee573a3a542c0d
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-04-03 12:00:31 +0300

    Add styles to welcome page

--- src/components/welcomePage/welcomePage.component.ts
+++ src/components/welcomePage/welcomePage.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   templateUrl: './welcomePage.component.html',
-  styleUrls: ['./welcomePage.component.scss']
+  styleUrls: ['./welcomePage.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class WelcomePageComponent {
 }
