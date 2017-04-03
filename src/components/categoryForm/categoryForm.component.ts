923fbc8b14412e4d8292a725ce73c30150e81aa5
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-04-03 13:04:15 +0300

    Add basic functionality and styles to create category page

--- src/components/categoryForm/categoryForm.component.ts
+++ src/components/categoryForm/categoryForm.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   templateUrl: './categoryForm.component.html',
-  styleUrls: ['./categoryForm.component.scss']
+  styleUrls: ['./categoryForm.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CategoryFormComponent {
+  subjects: { value: string, viewValue: string }[] = [
+    {value: 'language', viewValue: 'Українська мова'},
+    {value: 'literature', viewValue: 'Українська література'}
+  ];
+
   subject: string;
   category: string;
 
