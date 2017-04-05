aa828753d2be197cbb0539f9f194342ef8e2ed51
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-04-05 14:39:39 +0300

    Refactor code

--- src/components/toolbar/toolbar.component.ts
+++ src/components/toolbar/toolbar.component.ts
@@ -1,24 +1,9 @@
 import { Component } from '@angular/core';
 
-import { MdDialog, MdDialogRef } from '@angular/material';
-
-//import { dialogCreateCategory } from '../dialog-create-category/dialog-create-category.component';
-
 @Component({
   selector: 'llta-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
-  subject: string;
-  category: string;
-
-  constructor(public dialog: MdDialog) {}
-
-  //openDialog() {
-  //  let dialogRef = this.dialog.open(dialogCreateCategory);
-  //  dialogRef.afterClosed().subscribe(result => {
-  //
-  //  });
-  //}
 }
