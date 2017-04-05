b2f046b5af959a14cef8d462884ff1b44d4c12e0
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-04-05 16:32:43 +0300

    Refactor code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,42 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
-import { FooterComponent } from '../components/footer/footer.component';
-import { ToolbarComponent } from '../components/toolbar/toolbar.component';
-import { PageNotFoundComponent } from '../components/pageNotFound/pageNotFound.component';
-import { WelcomePageComponent } from '../components/welcomePage/welcomePage.component';
 import { CategoriesListComponent } from '../components/categoriesList/categoriesList.component';
-import { TestsListComponent } from '../components/testsList/testsList.component';
-import { TestFormComponent } from '../components/testForm/testForm.component';
 import { CategoryFormComponent } from '../components/categoryForm/categoryForm.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { PageNotFoundComponent } from '../components/pageNotFound/pageNotFound.component';
+import { TestFormComponent } from '../components/testForm/testForm.component';
+import { TestsListComponent } from '../components/testsList/testsList.component';
+import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+import { WelcomePageComponent } from '../components/welcomePage/welcomePage.component';
 
 import { appRoutes } from './app.routes';
 
 @NgModule({
   imports: [
     BrowserModule,
-    HttpModule,
     FormsModule,
+    HttpModule,
     MaterialModule,
     RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     AppComponent,
-    PageNotFoundComponent,
+    CategoriesListComponent,
+    CategoryFormComponent,
     FooterComponent,
+    PageNotFoundComponent,
+    TestFormComponent,
+    TestsListComponent,
     ToolbarComponent,
     WelcomePageComponent,
-    CategoriesListComponent,
-    TestsListComponent,
-    TestFormComponent,
-    CategoryFormComponent,
   ],
   bootstrap: [
     AppComponent,
