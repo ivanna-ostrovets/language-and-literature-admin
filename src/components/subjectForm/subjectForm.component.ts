2db4d9c9023ade7bd9cc678534a19cbb9cf3a043
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-04-12 11:33:23 +0300

    Add model and service to subject component

--- src/components/subjectForm/subjectForm.component.ts
+++ src/components/subjectForm/subjectForm.component.ts
@@ -1,17 +1,20 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
+import { Subject } from '../../models/subject';
+
+import { SubjectService } from '../../services/subject.service';
+
 @Component({
   templateUrl: './subjectForm.component.html',
   styleUrls: ['./subjectForm.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class SubjectFormComponent {
-  subject: string;
+  constructor(private subjectService: SubjectService) {}
 
-  onSubmit(): void {
-    let subject = {
-      id: 'subject_',
-      subject: this.subject
-    };
+  subject: Subject = new Subject();
+
+  submit() {
+    this.subject = new Subject();
   }
 }
