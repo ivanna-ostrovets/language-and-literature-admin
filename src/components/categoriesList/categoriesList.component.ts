26e9454791218653f4fd096c8c925587d02b0af2
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-04-19 17:31:41 +0300

    Add all subjects view

--- src/components/categoriesList/categoriesList.component.ts
+++ src/components/categoriesList/categoriesList.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Category } from '../../models/category';
+import { Subject } from '../../models/subject';
+
+import { CategoryService } from '../../services/category.service';
+import { SubjectService } from '../../services/subject.service';
 
 @Component({
   templateUrl: './categoriesList.component.html',
   styleUrls: ['./categoriesList.component.scss']
 })
-export class CategoriesListComponent {
+export class CategoriesListComponent implements OnInit {
+  categories: Category[] = [];
+  subjects: Subject[] = [];
+  subject: Subject = new Subject();
 
+  constructor(
+    private categoryService: CategoryService,
+    private subjectService: SubjectService
+  ) {
+  }
+
+  ngOnInit() {
+    this.subjectService.getAll().then(subjects => {
+      this.subjects = subjects;
+    });
+  }
+
+  delete(categoryId: string) {
+
+  }
 }
