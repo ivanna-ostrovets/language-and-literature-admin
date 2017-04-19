d1abd39a4f393432209e9b4193f0d2465f273009
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-04-19 17:44:22 +0300

    Add all subjects view

--- src/components/subjectsList/subjectsList.component.ts
+++ src/components/subjectsList/subjectsList.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Subject } from '../../models/subject';
+
+import { SubjectService } from '../../services/subject.service';
 
 @Component({
   templateUrl: './subjectsList.component.html',
   styleUrls: ['./subjectsList.component.scss']
 })
-export class SubjectsListComponent {
+export class SubjectsListComponent implements OnInit {
+  subjects: Subject[] = [];
 
+  constructor(private subjectService: SubjectService) {
+  }
+
+  ngOnInit() {
+    this.subjectService.getAll().then(subjects => {
+      this.subjects = subjects;
+    });
+  }
+
+  delete(subjectId: string) {
+
+  }
 }
