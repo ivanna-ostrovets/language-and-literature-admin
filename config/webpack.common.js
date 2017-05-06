6093601eb0b96c374d3f4b9c62dff833d10490bd
Yury Kozhenov <yurykozhenov@gmail.com>
2017-05-06 22:10:42 +0300

    Fix some issues

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -26,11 +26,11 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file-loader?name=assets/[name].[hash].[ext]-loader'
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
       },
       {
         test: /\.json$/,
-        loader: 'file-loader?name=mocks/[name].[ext]-loader'
+        loader: 'file-loader?name=mocks/[name].[ext]'
       },
       {
         test: /\.css$/,
