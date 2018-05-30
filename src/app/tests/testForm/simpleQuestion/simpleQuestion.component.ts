f93ebd8ec22b0183c822eec73eeb82f14c4982ea
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2018-05-30 03:59:49 +0300

    Fix bugs

--- src/app/tests/testForm/simpleQuestion/simpleQuestion.component.ts
+++ src/app/tests/testForm/simpleQuestion/simpleQuestion.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { SimpleQuestion } from '../../../../shared/models/simpleQuestion.model';
 import { resizeArray } from '../../../../shared/utils/resizeArray';
@@ -8,10 +8,15 @@ import { resizeArray } from '../../../../shared/utils/resizeArray';
   templateUrl: './simpleQuestion.component.html',
   styleUrls: ['./simpleQuestion.component.scss'],
 })
-export class SimpleQuestionComponent {
+export class SimpleQuestionComponent implements OnInit {
   @Input() question: SimpleQuestion;
   @Input() questionIndex: number;
   @Input() selectedIndex: number;
+
+  ngOnInit() {
+    const questionsQuantity = 5;
+    this.onAnswersQuantityChange(questionsQuantity, this.question);
+  }
 
   onAnswersQuantityChange(quantity: number, question: SimpleQuestion) {
     resizeArray(question.simpleQuestionAnswers, quantity, () => ({ text: null }));
